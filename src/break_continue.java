97d1d2feec74ba89f63544264eb7f43edfedb980
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-04-15 15:52:58 -0400

    Add a break_continue.java

---
+++ src/break_continue.java
@@ -0,0 +1,19 @@
+public class break_continue {
+    public static void main(String[] args){
+
+        //break = break out of a loop (STOP)
+        // continue = skip current iteration of a loop (SKIP)
+
+        for(int i = 0; i < 20; i++) {
+
+            if (i == 5) {
+                continue;
+            }
+            if (i == 15) {
+                break;
+            }
+
+            System.out.print(i + " ");
+        }
+    }
+}
